48b65724ce6264d4be1ae64aac2dda98dbf4c120
Kelvin-Squishy <obengappiah98@gmail.com>
2022-09-12 21:39:16 +0800

    squash bugs

--- pages/index.js
+++ pages/index.js
@@ -2,22 +2,5 @@ import styles from '../styles/Home.module.css';
 import { useState } from 'react';
 
 export default function Home() {
-  const [title, setTitle] = useState('Initial Title');
-
-  const handleClick = () => {
-    if (title === 'Initial Title') {
-      setTitle('Hello useState');
-    } else {
-      setTitle('Initial Title');
-    }
-  };
-
-  return (
-    <section>
-      <h1 className={styles.section}> {title} </h1>
-      <button className={styles.button} onClick={handleClick}>
-        Change title
-      </button>
-    </section>
-  );
+  return <h1>Hey</h1>;
 }
